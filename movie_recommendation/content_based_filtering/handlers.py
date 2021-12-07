a446061b7f5422687ad23b5f8a825fc5e03ef4da
tandavam <sachinsrini5@gmail.com>
2021-12-06 23:41:22 -0500

    read csv

--- movie_recommendation/content_based_filtering/handlers.py
+++ movie_recommendation/content_based_filtering/handlers.py
@@ -1,7 +1,16 @@
+import pandas as pd
+import os.path
+
+
 class ContentBasedFiltering:
     def __init__(self):
-        self.credits_dataset = "../datasets/tmdb_5000_credits.csv"
-        self.movies_dataset = "../datasets/tmdb_5000_movies.csv"
+        self.path = os.path.abspath(os.path.dirname(__file__))
+        self.credits_dataset_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "../datasets"
+                                                                                             "/tmdb_5000_credits.csv")
+        self.movies_dataset_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "../datasets"
+                                                                                            "/tmdb_5000_movies.csv")
+        self.credits_dataset = pd.read_csv(self.credits_dataset_path)
+        self.movies_dataset = pd.read_csv(self.movies_dataset_path)
 
     def get_the_recommended_movies(self, movie_data):
         return {
